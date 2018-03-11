b4a07e84c66c6d428763ededfebb559c3067fdbe
SorenAdrieux <Soren@emilys-imac.attlocal.net>
2018-03-11 13:19:13 -0400

    memory-game

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 var cards = ["queen", "queen", "king", "king"]
 var cardsInPlay = []
-var cardOne = cards[0]
-var cardTwo = cards[2]
-cardsInPlay.push('cardOne');
-cardsInPlay.push('cardTwo');
-if (cardsInPlay.length < 2) {
-	alert("Pick a second card!")
-} else {
-	if (cardOne === cardTwo) {
-		alert("You found a match!");
+var checkForMatch = function () {
+	if (cardsInPlay.length < 2) {
+		alert("Pick a second card!")
 	} else {
-		alert("Sorry, try again.");
-	}
-}
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			console.log("You found a match!");
+		} else {
+			console.log("Sorry, try again.");
+		};
+	};
+};
+var flipCard = function (cardId) {
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId]);
+};
+flipCard(0);
+flipCard(2);
